9ebdb70fca954227727ba4b1e89980e4fe08591f
Kaushal Powar <90775147+kaushalpowar@users.noreply.github.com>
2023-11-21 05:46:32 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from dotenv import load_dotenv
 
+# Load environment variables from .env
+load_dotenv()
+
+# Access the environment variable
+streamlit_server_max_upload_size = int(os.getenv("STREAMLIT_SERVER_MAX_UPLOAD_SIZE", default=1000))
 st.set_page_config('Home', '📖')
 st.title('Docusphere HomePage 📖')
