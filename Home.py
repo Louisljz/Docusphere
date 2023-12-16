6ce4f235eb67f96374fc8d5040529947ea2e1204
kaushal powar <knp1532001@gmail.com>
2023-12-16 20:41:08 +0530

    Add selectbox to choose model, either OpenAI or Huggingface

--- Home.py
+++ Home.py
@@ -14,12 +14,22 @@ st.set_page_config('Home', '📖')
 st.title('Docusphere')
 st.write('Connect your Pinecone VectorDB with OpenAI Embedding Option.')
 
-api_key = st.text_input('API Key:', type='password')
-env = st.text_input('Environment:')
-index_name = st.text_input('Index Name:')
+option = st.selectbox('Select a model:', [' ','OpenAI', 'Huggingface'], index=0 ,placeholder='Select a model')
 
-if api_key and env and index_name and st.button('connect'): 
-    pinecone.init(api_key=api_key, environment=env)
-    st.session_state.vector_store = Pinecone.from_existing_index(index_name, OpenAIEmbeddings())
-    st.success('Pinecone DB connected!')
-    switch_page('Ingest Documents')
+if option == 'OpenAI':
+    api_key = st.text_input('API Key:', type='password')
+    env = st.text_input('Environment:')
+    index_name = st.text_input('Index Name:')
+
+    if api_key and env and index_name and st.button('connect', use_container_width=True): 
+        pinecone.init(api_key=api_key, environment=env)
+        st.session_state.vector_store = Pinecone.from_existing_index(index_name, OpenAIEmbeddings())
+        st.success('Pinecone DB connected!')
+        switch_page('Ingest Documents')
+
+
+elif option == 'Huggingface':
+    st.info('Coming soon!')
+
+else:
+    st.info('Select a model to continue 👆')
